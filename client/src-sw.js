16fbaf9f8070972ae29778599d6a08f1e25f02b0
microvac23 <Ithanmachuca@gmail.com>
2023-09-02 18:22:13 -0500

    added more logic to database, install, etc

--- client/src-sw.js
+++ client/src-sw.js
@@ -45,19 +45,3 @@ registerRoute(
     ],
   })
 );
-
-registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'my-image-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  })
-);
