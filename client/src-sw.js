452c3320cadfcb4995a2e014583fb89545998073
microvac23 <Ithanmachuca@gmail.com>
2023-09-02 17:43:57 -0500

    app runs in browser, still some errors

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 registerRoute(
   ({ request }) => {
     console.log(request);
